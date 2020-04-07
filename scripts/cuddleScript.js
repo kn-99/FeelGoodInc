481193d26ff5c280fe4ae88d50dbfe4d8cfec6dc
Jameson Hale <halejameson@gmail.com>
2020-04-07 13:07:26 -0400

    added get help now alert to nav bar

--- scripts/cuddleScript.js
+++ scripts/cuddleScript.js
@@ -1,9 +1,13 @@
 function getHelpNow() {
     console.log('Help requested');
+    alert("We're here for you <3 Give us a call\n000-000-0000")
+
+    /*
     var x = document.getElementById("help-now");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
+    */
 } 
